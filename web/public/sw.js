8120cc391d8ec67cd8d3f48e4992aab7c2c7e378
Echo Sun <sqy6962017@gmail.com>
2026-08-22 01:44:29 +1200

    Rebuild mobile viewport shell

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'geko-keeper-v2';
+const CACHE_NAME = 'geko-keeper-v3';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
